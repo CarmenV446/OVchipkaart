9c9779953654efe32bee92dfdcd8ffd8412f3168
CarmenV446 <1220871@student.roc-nijmegen.nl>
2026-03-31 12:57:43 +0200

    connection made
    made first code of ov pas and kaartlezer, made connection to git

---
+++ src/ovchipkaart/OVpas.java
@@ -0,0 +1,40 @@
+package ovchipkaart;
+
+public class OVpas {
+	private double saldo;
+	private boolean status;
+	private int huidigeKM;
+	private int pasnummer;
+	private int vervaljaar;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public boolean getStatus() {
+		return status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	public int getHuidigKM() {
+		return huidigeKM;
+	}
+	
+	public int getPasnummer() {
+		return pasnummer;
+	}
+	public int getVervaljaar() {
+		return vervaljaar;
+		
+	}
+
+	
+	
+}
---
+++ src/ovchipkaart/ovchipkaart.java
@@ -0,0 +1,10 @@
+package ovchipkaart;
+
+public class ovchipkaart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
