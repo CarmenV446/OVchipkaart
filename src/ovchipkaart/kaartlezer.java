ac63e4dab175a8e20ce2f92cf95df482441e211e
CarmenV446 <1220871@student.roc-nijmegen.nl>
2026-04-10 09:09:34 +0200

    checking in functions made
    i made the code based on the class diagram that i had made previously but some were left out due to needing more information that are based on checking out

--- src/ovchipkaart/kaartlezer.java
+++ src/ovchipkaart/kaartlezer.java
@@ -1,5 +1,41 @@
 package ovchipkaart;
 
 public class kaartlezer {
+	private double sumsaldo;
+	private int reisKM;
+	private int nieuwKM;
+	
+	
+	public boolean saldoCheck(OVpas pas) {
+		if (pas.getSaldo() < 15) {
+		return false;
+		}
+		else {
+			return true;
+		}
 
+	}
+	public boolean statusCheck(OVpas pas) {
+		if  (pas.getStatus() == true) {
+			return false;
+			//als status op true staat dan betekent dat hij ingecheckt is 
+			//dus dan krijgt hij een false terug want dan mag hij niet verder
+		}
+		else {
+			return true;
+		}
+	}
+	
+	public boolean pasCheck(OVpas pas) {
+		if (pas.getPasnummer() == 1); {
+			return true;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 }
